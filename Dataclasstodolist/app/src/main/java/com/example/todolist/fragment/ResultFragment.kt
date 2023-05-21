7aa8181b31212ce045dc828bf3f5eb0eef8404ac
Yana <0roves-pretend@icloud.com>
2023-05-21 17:19:08 +0900

    Task 수정 및 삭제 기능 추가

--- Dataclasstodolist/app/src/main/java/com/example/todolist/fragment/ResultFragment.kt
+++ Dataclasstodolist/app/src/main/java/com/example/todolist/fragment/ResultFragment.kt
@@ -13,7 +13,7 @@ import androidx.fragment.app.Fragment
 import com.example.todolist.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-data class TodoItem(val title: String, val task: String, var priorityNum: Int)
+data class TodoItem(var title: String, var task: String, var priorityNum: Int)
 
 class ResultFragment : Fragment() {
 
@@ -102,14 +102,9 @@ class ResultFragment : Fragment() {
         ArrayAdapter<TodoItem>(context, 0, data) {
 
         override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-
-            // convertView를 재사용하거나 새로 생성하여 View 객체를 반환합니다.
             val view = convertView ?: LayoutInflater.from(context).inflate(R.layout.list_item_layout, parent, false)
-
-            // 현재 position에 해당하는 데이터를 가져옵니다.
             val item = todoList[position]
 
-            // 뷰에 데이터를 표시합니다.
             val titleView = view.findViewById<TextView>(R.id.todoTitleView)
             titleView.text = item.title
             titleView.setTextColor(ContextCompat.getColor(context, R.color.black))
@@ -118,15 +113,98 @@ class ResultFragment : Fragment() {
             textView.text = item.task
             textView.setTextColor(ContextCompat.getColor(context, R.color.black))
 
-            titleView.setOnClickListener {
-
-                // 클릭한 항목 삭제
-                todoList.removeAt(position)
-
-                // 어댑터 갱신
-                notifyDataSetChanged()
+            val editButton = view.findViewById<ImageButton>(R.id.editButton)
+            editButton.setOnClickListener {
+                showEditDeleteDialog(position)
             }
+
             return view
+        }
+
+        private fun showEditDeleteDialog(position: Int) {
+            val options = arrayOf("수정", "삭제")
+            val dialogBuilder = AlertDialog.Builder(context)
+            dialogBuilder.setTitle("옵션 선택")
+                .setItems(options) { dialog, which ->
+                    when (which) {
+                        0 -> showEditDialog(position)
+                        1 -> deleteItem(position)
+                    }
+                }
+            val alertDialog = dialogBuilder.create()
+            alertDialog.show()
+        }
+
+        private fun showEditDialog(position: Int) {
+            val item = todoList[position]
+
+            val dialogView = LayoutInflater.from(context).inflate(R.layout.dialog_0, null)
+            val titleEditText = dialogView.findViewById<EditText>(R.id.editTitle)
+            val taskEditText = dialogView.findViewById<EditText>(R.id.editTask)
+            val priorityNumSpinner = dialogView.findViewById<Spinner>(R.id.priorityNumSpinner)
+
+            titleEditText.setText(item.title.substringBeforeLast("-").trim())
+            taskEditText.setText(item.task)
+
+            val adapter = ArrayAdapter(
+                context,
+                android.R.layout.simple_spinner_item,
+                priorityOptions
+            )
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            priorityNumSpinner.adapter = adapter
+
+            priorityNumSpinner.setSelection(item.priorityNum - 1)
+
+            val dialogBuilder = AlertDialog.Builder(context)
+                .setView(dialogView)
+                .setTitle("할 일 수정")
+
+            val okButton = dialogView.findViewById<Button>(R.id.successButton)
+            val alertDialog = dialogBuilder.create()
+            okButton.setOnClickListener {
+                // 수정된 내용 적용
+                val editedTitle = titleEditText.text.toString()
+                val editedTask = taskEditText.text.toString()
+                val editedPriorityNum = priorityNumSpinner.selectedItem.toString().toInt()
+
+                item.title = ("${editedTitle} - ${editedPriorityNum}")
+                item.task = editedTask
+                item.priorityNum = editedPriorityNum
+
+                // todoList를 우선순위에 따라 소팅
+                todoList.sortBy { it.priorityNum }
+
+                // 데이터 갱신
+                notifyDataSetChanged()
+                Toast.makeText(context, "할 일이 수정되었습니다.", Toast.LENGTH_SHORT).show()
+
+                // 다이얼로그 닫기
+                alertDialog.dismiss()
+            }
+            alertDialog.show()
+
+            val noButton = dialogView.findViewById<Button>(R.id.closeButton)
+            noButton.setOnClickListener {
+                alertDialog.dismiss()
+            }
+        }
+
+        private fun deleteItem(position: Int) {
+            val item = todoList[position]
+
+            // 삭제 확인 팝업 띄우기
+            val alertDialogBuilder = AlertDialog.Builder(context)
+            alertDialogBuilder.setTitle("삭제 확인")
+            alertDialogBuilder.setMessage("Task를 삭제하시겠습니까?")
+            alertDialogBuilder.setPositiveButton("예") { dialog, which ->
+                // 사용자가 확인을 선택한 경우
+                todoList.removeAt(position)
+                notifyDataSetChanged()
+                Toast.makeText(context, "할 일이 삭제되었습니다.", Toast.LENGTH_SHORT).show()
+            }
+            alertDialogBuilder.setNegativeButton("아니오", null)
+            alertDialogBuilder.show()
         }
     }
 }
